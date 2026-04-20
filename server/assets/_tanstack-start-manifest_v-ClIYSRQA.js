027044755fe719c194c54dd6c6cc4c07281ae069
WesleySilva373 <WesleySilva373@users.noreply.github.com>
2026-04-20 23:22:56 +0000

    deploy: ddae9bf23a1e9cdd78b4cbbba5b70aec4c75c559

---
+++ server/assets/_tanstack-start-manifest_v-ClIYSRQA.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/home/runner/work/port-definitivo/port-definitivo/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/port-definitivo/assets/index-BMmRK6i8.js"] }, "/": { filePath: "/home/runner/work/port-definitivo/port-definitivo/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/port-definitivo/assets/index-B0Ke7sSD.js"] } }, clientEntry: "/port-definitivo/assets/index-BMmRK6i8.js" });
+export {
+  tsrStartManifest
+};
